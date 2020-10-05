61536d6d3409562662adb518205186e677bced9e
frezier <freziern@gmail.com>
2020-10-05 12:14:31 +0300

    psr-4 complience

--- routes/api.php
+++ routes/api.php
@@ -46,7 +46,9 @@ Route::group(["middleware" => "api"], function(){
 
 });
 
+
+ */
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
- */
