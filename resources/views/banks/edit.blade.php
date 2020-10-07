1bab63ce51962b20dc8f270acde9cbd3aad6c5b1
frezier <freziern@gmail.com>
2020-10-07 07:45:32 +0300

    psr-4 complience

--- resources/views/banks/edit.blade.php
+++ resources/views/banks/edit.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="col-md-6">
     <div class="card">
-        <div class="card-header">{{ __('messages.edit').' '.__('messages.role')}}</div>
+        <div class="card-header">{{ __('messages.edit').' '.__('messages.bank')}}</div>
 
         <div class="card-body">
-            {!! Form::open(['action' => array('Payroll\BankController@update', $role->id),'method' => 'PUT']) !!}
+            {!! Form::open(['action' => array('Payroll\BankController@update', $bank->id),'method' => 'PUT']) !!}
 
-            {{ Form::bsText('name', $role->name,['placeholder' => __('messages.enter name')]) }}
+            {{ Form::bsText('name', $bank->name,['placeholder' => __('messages.enter name')]) }}
 
-            {{ Form::bsText('description', $role->description ,['placeholder' => __('messages.enter description')]) }}
+            {{ Form::bsText('description', $bank->description ,['placeholder' => __('messages.enter description')]) }}
 
             {{ Form::bsSubmit(__('messages.edit'),['class' => 'btn btn-primary']) }}
 
