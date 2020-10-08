77fe239642648e2b8eaf49b143bcd947960cbca6
frezier <freziern@gmail.com>
2020-10-08 06:22:09 +0300

    psr-4 complience

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,6 +10,140 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    protected $settings = [
+      [
+          'key'                       =>  'site_name',
+          'value'                     =>  'BrainySoft',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'site_title',
+          'value'                     =>  'BrainySoft',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'default_email_address',
+          'value'                     =>  'yahaya.frezier@datahousetza.com',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'currency_code',
+          'value'                     =>  'TZS',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'currency_symbol',
+          'value'                     =>  'Tsh',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'site_logo',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'site_favicon',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'footer_copyright_text',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'seo_meta_title',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'seo_meta_description',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'social_facebook',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'social_twitter',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'social_instagram',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'social_linkedin',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'google_analytics',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'facebook_pixels',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'stripe_payment_method',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'stripe_key',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'stripe_secret_key',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'paypal_payment_method',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'paypal_client_id',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+      [
+          'key'                       =>  'paypal_secret_id',
+          'value'                     =>  '',
+          'company_id'                =>   1,
+          'description'               =>  '',
+      ],
+  ];
     public function run()
     {
       $this->call('LevelsTableSeeder');   
