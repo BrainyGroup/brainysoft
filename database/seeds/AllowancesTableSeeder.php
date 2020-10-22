0fe7799a8e2564546aaa252806ef23136d92f4cc
frezier <freziern@gmail.com>
2020-10-22 11:37:12 +0300

    psr-4 complience

--- database/seeds/AllowancesTableSeeder.php
+++ database/seeds/AllowancesTableSeeder.php
@@ -788,7 +788,7 @@ class AllowancesTableSeeder extends Seeder
                 // atache all permissions to admin role
                 $operator_role->permissions()->attach([1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,1]);
 
-                $employer_role = Role::where('name','operator')->first();
+                $employer_role = Role::where('name','employer')->first();
                 // atache all permissions to admin role
                 $employer_role->permissions()->attach([1,2,3,4,5,6,7,8,9,11,12,13,22]);
 
@@ -798,7 +798,7 @@ class AllowancesTableSeeder extends Seeder
                 $employee_role->permissions()->attach([1,2,3,4,5,6,7,8,9,12,13, 246, 250, 88, 224, 261, 262]);
 
 
-                $user_role = Role::where('name','operator')->first();
+                $user_role = Role::where('name','user')->first();
                 // atache all permissions to admin role
                 $user_role->permissions()->attach([1,2,3,4,5,6,7,8,9,12,13]);
         
