9ff740b17fba534c741a3afde6611dea8943305d
frezier <freziern@gmail.com>
2021-08-16 16:04:30 +0300

    test update to remote

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-success text-uppercase mb-1">{{ __('messages.earning annually') }}</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">TZS <count-up :to="{{$annual_net}}"></div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">TZS2 <count-up :to="{{$annual_net}}"></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
